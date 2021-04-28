2970b7f4396bf5225b6dd4f663eefe4b538f7892
Carlos Alexandre de Souza Braga <casbalexandre@hotmail.com>
2021-04-28 11:48:46 -0300

    Criadas as opções

---
+++ PrimeiroSistema/Aluno.cs
@@ -0,0 +1,11 @@
+namespace PrimeiroSistema
+{
+    public struct Aluno // Testando Struct (ideal seria class)
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+
+        
+    }
+}
--- PrimeiroSistema/Program.cs
+++ PrimeiroSistema/Program.cs
@@ -6,7 +6,52 @@ namespace PrimeiroSistema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Aluno[] alunos = new Aluno [5];
+            string opcaousuario = ObterOpcaoUsuario();
+
+            while (opcaousuario.ToUpper() != "X")
+            {
+                switch (opcaousuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        Console.Write("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        aluno.Nota = decimal.Parse(Console.ReadLine());
+                        // 10:42
+                        break;
+
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+
+                    case "3":
+                        //TODO: calcular media geral
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaousuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaousuario = Console.ReadLine();
+            return opcaousuario;
         }
     }
 }
